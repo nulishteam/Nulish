f4ecca1ed42412fb95eea3b6bcaec4f160caf5e0
Ade Ramdani <mr.aderamdani@gmail.com>
2023-09-20 09:21:46 +0700

    Sudah diperbaiki untuk form action untuk mengirim data ke Controller dan database, tapi masih error ketika submit dengan error "The POST method is not supported for route contact. Supported methods: GET, HEAD."

--- app/Http/Controllers/ContactController.php
+++ app/Http/Controllers/ContactController.php
@@ -22,10 +22,41 @@ class ContactController extends Controller
             'subject' => 'required',
             'message' => 'required',
         ]);
-        dd($request);
-        //  Store data in database
-        Contact::create($request->all());
-        //
-        return back()->with('success', 'We have received your message and would like to thank you for writing to us.');
+
+        // Get the form data
+        $name = $request->input('name');
+        $email = $request->input('email');
+        $phone = $request->input('phone');
+        $subject = $request->input('subject');
+        $message = $request->input('message');
+
+        // Save the form data to the database
+        $contact = Contact::create([
+            'name' => $name,
+            'email' => $email,
+            'phone' => $phone,
+            'subject' => $subject,
+            'message' => $message,
+        ]);
+
+        // Send a confirmation email to the user
+        $to = $email;
+        $subject = "Contact Form Submission Confirmation";
+        $message = "Dear $name,
+
+        Thank you for contacting us. Your message has been received and will be reviewed by our team.
+
+        We will get back to you as soon as possible.
+
+        Sincerely,
+        The Nulish Team";
+
+        mail($to, $subject, $message);
+
+        // Flash a success message to the session
+        session()->flash('success', 'Your message has been sent!');
+
+        return redirect()->route('contact.store');
+
     }
 }
